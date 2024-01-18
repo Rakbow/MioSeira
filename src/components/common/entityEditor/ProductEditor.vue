051482fdca8acbffb16c00a5c2a59c543feb8760
Rakbow <rakbow@gmail.com>
2024-01-19 02:32:03 +0800

    product editor add

---
+++ src/components/common/entityEditor/ProductEditor.vue
@@ -0,0 +1,103 @@
+<script setup>
+import {onMounted, ref, inject, getCurrentInstance} from "vue";
+import {AxiosHelper as axios} from '@/utils/axiosHelper';
+import {useToast} from "primevue/usetoast";
+import {useDialog} from 'primevue/usedialog';
+import {API} from '@/config/Web_Helper_Strs';
+const $const = getCurrentInstance().appContext.config.globalProperties.$const;
+
+onMounted(() => {
+  item.value = JSON.parse(JSON.stringify(dialogRef.value.data.item));
+  option.value = dialogRef.value.data.option;
+  init();
+});
+
+const toast = useToast();
+const dialog = useDialog();
+const dialogRef = inject("dialogRef");
+const isUpdate = ref(false);
+const editBlock = ref(false);
+const item = ref({});
+const option = ref({});
+
+const init = () => {
+  item.value.category = item.value.category.value;
+  item.value.franchise = item.value.franchise.value;
+}
+
+const submit = () => {
+  editBlock.value = true;
+  axios.post(API.UPDATE_PRODUCT, item.value)
+      .then(res => {
+        if (res.state === axios.SUCCESS) {
+          toast.add({severity: 'success', detail: res.message, life: 3000});
+          isUpdate.value = true;
+          close();
+        } else {
+          toast.add({severity: 'error', detail: res.message, life: 3000});
+        }
+        editBlock.value = false;
+      })
+}
+
+const close = () => {
+  dialogRef.value.close(
+      {
+        isUpdate: isUpdate.value
+      }
+  );
+}
+</script>
+
+<template>
+  <Toast/>
+  <BlockUI :blocked="editBlock" class="p-fluid">
+    <div class="formgrid grid">
+      <div class="field col">
+        <label>{{$const.Name}}<span style="color: red">*</span></label>
+        <InputText id="name" v-model="item.name" />
+      </div>
+      <div class="field col">
+        <label>{{$const.NameEn}}</label>
+        <InputText id="nameEn" v-model="item.nameEn" />
+      </div>
+      <div class="field col">
+        <label>{{$const.NameZh}}</label>
+        <InputText id="nameZh" v-model="item.nameZh" />
+      </div>
+    </div>
+    <div class="formgrid grid">
+      <div class="field col-6">
+        <label>{{$const.ReleaseDate}}<span style="color: red">*</span></label>
+        <Calendar id="releaseDate" v-model="item.releaseDate" dateFormat="yy/mm/dd"
+                  :showButtonBar="true"
+                  :showIcon="true" />
+      </div>
+      <div class="field col-3">
+        <label>{{$const.Category}}</label>
+        <Dropdown v-model="item.category" :options="option.productCategorySet"
+                  optionLabel="label" optionValue="value" />
+      </div>
+    </div>
+    <div class="formgrid grid">
+      <div class="field col-3">
+        <label>{{$const.Franchise}}</label>
+        <Dropdown v-model="item.franchise" :options="option.productCategorySet"
+                  optionLabel="label" optionValue="value" />
+      </div>
+    </div>
+    <div class="field">
+      <label>{{$const.Remark}}</label>
+      <Textarea id="remark" v-model="item.remark" rows="3" cols="20" :autoResize="true" />
+    </div>
+    <div class="text-end mt-3 mb-2">
+      <Button icon="pi pi-times" :label="$const.Cancel" @click="close"
+              class="p-button-text" />
+      <Button icon="pi pi-save" :label="$const.Save" @click="submit" />
+    </div>
+  </BlockUI>
+</template>
+
+<style scoped>
+
+</style>
