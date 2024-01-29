9fb61300bf2a0fa73be1c83632c831b89e090064
Rakbow <rakbow@gmail.com>
2024-01-30 00:19:20 +0800

    引入franchise模块

---
+++ src/views/detail/FranchiseDetail.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="main" class="grid mt-2">
+    <Toast></Toast>
+    <div class="detail-card col-8 lg:col-offset-1">
+      <Card>
+        <template #title>
+          <div class="grid detail-item-header-title">
+            <h4 class="col-11">
+              <b class="detail-item-title">
+                {{ item.name }}
+              </b>
+            </h4>
+          </div>
+        </template>
+        <template #subtitle>
+          <h5 v-if="item.nameEn !== ''" class="detail-item-subtitle"><small>{{ item.nameEn }}</small></h5>
+          <h5 v-else><small></small></h5>
+          <h5 v-if="item.nameZh !== ''" class="detail-item-subtitle"><small>{{ item.nameZh }}</small></h5>
+          <h5 v-else><small></small></h5>
+        </template>
+        <template #content>
+          <div class="grid">
+            <div class="col-4" style="width: 205px">
+              <div class="item-detail-cover">
+                <img :src="itemImageInfo.cover.url" :alt="itemImageInfo.cover.name"/>
+              </div>
+            </div>
+            <div class="col detail-item-header-card">
+              <Card>
+                <template #content>
+                  <div class="relative">
+                    <div v-if="userStore.user">
+                      <Button v-if="userStore.user.type === 0" class="p-button-link absolute top-0"
+                              @click="openEditDialog" style="right: 25%"
+                              v-tooltip.bottom="{value: $const.Edit, class: 'short-tooltip'}" >
+                        <template #icon>
+                          <span class="material-symbols-outlined">edit_note</span>
+                        </template>
+                      </Button>
+                    </div>
+                    <Info :item="item" />
+                    <StatusEditor :status="item.status" />
+                    <ItemLike :likeCount="pageInfo.likeCount" :liked="pageInfo.liked" />
+                  </div>
+                </template>
+              </Card>
+            </div>
+          </div>
+          <div class="detail-item-field">
+            <!-- detail -->
+            <DetailPad :header="$const.Description" :text="item.detail" />
+          </div>
+        </template>
+      </Card>
+    </div>
+    <div class="col-2" style="min-width: 300px">
+      <SideImages :images="itemImageInfo"/>
+      <TrafficInfo :info="pageInfo" :addedTime="item.addedTime" :editedTime="item.editedTime" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/item-detail.css';
+import '@/assets/bootstrap/myBootstrap.min.css';
+import '@/lib/bootstrap.bundle.min';
+import {getCurrentInstance, onBeforeMount, ref} from "vue";
+import {useRouter} from "vue-router";
+import {useToast} from "primevue/usetoast";
+import {useUserStore} from "@/store/user";
+import {useDialog} from "primevue/usedialog";
+import SideImages from "@/components/common/SideImages.vue";
+import TrafficInfo from "@/components/common/PageTraffic.vue";
+import DetailPad from "@/components/common/DetailPad.vue";
+import StatusEditor from "@/components/common/StatusEditor.vue";
+import ItemLike from "@/components/common/ItemLike.vue";
+import Info from "@/views/detail/FranchiseDetailInfo.vue";
+import InfoEditor from "@/components/common/entityEditor/FranchiseInfoEditor.vue";
+
+const $const = getCurrentInstance().appContext.config.globalProperties.$const;
+const router = useRouter();
+const toast = useToast();
+const userStore = useUserStore();
+const dialog = useDialog();
+
+const item = ref({});
+const pageInfo = ref({});
+const itemImageInfo = ref({});
+const option = ref({});
+const personnel = ref([]);
+
+onBeforeMount(() => {
+  item.value = router.currentRoute.value.meta.info.item;
+  pageInfo.value = router.currentRoute.value.meta.info.traffic;
+  itemImageInfo.value = router.currentRoute.value.meta.info.itemImageInfo;
+  option.value = router.currentRoute.value.meta.info.options;
+  personnel.value = router.currentRoute.value.meta.info.personnel;
+});
+
+const openEditDialog = () => {
+  dialog.open(InfoEditor, {
+    props: {
+      header: $const.Edit,
+      style: {
+        width: '400px',
+      },
+      breakpoints:{
+        '960px': '60vw',
+        '640px': '50vw'
+      },
+      modal: true,
+      closable: false
+    },
+    data: {
+      item: item.value,
+      option: option.value,
+    },
+    onClose: (options) => {
+      if(options.data !== undefined) {
+        if(options.data.isUpdate) {
+          location.reload();
+        }
+      }
+    }
+  });
+}
+
+</script>
+
+<style scoped>
+
+.item-detail-cover {
+  transform: translateY(0);
+  /*width: 170px;*/
+  height: 185px;
+}
+</style>
---
+++ src/views/detail/FranchiseDetailInfo.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {defineProps, getCurrentInstance} from "vue";
+const $const = getCurrentInstance().appContext.config.globalProperties.$const;
+
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+  <table class="table-borderless table-sm ml-2">
+    <tbody class="detail-item-header-table">
+      <tr>
+        <td>
+          <i class="pi material-symbols-outlined">tag</i>
+          <strong>{{ $const.Name }}</strong>
+        </td>
+        <td>
+          {{ item.name }}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i class="pi material-symbols-outlined">tag</i>
+          <strong>{{ $const.NameZh }}</strong>
+        </td>
+        <td>
+          {{ item.nameZh }}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i class="pi material-symbols-outlined">tag</i>
+          <strong>{{ $const.NameEn }}</strong>
+        </td>
+        <td>
+          {{ item.nameEn }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+
+</style>
