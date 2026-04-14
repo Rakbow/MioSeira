f832f6f5b16204b4e8ef18f2bd6d9282815657d9
Rakbow <rakbow@gmail.com>
2026-04-15 01:06:12 +0800

    optimize index detail style

--- src/views/detail/IndexDetail.vue
+++ src/views/detail/IndexDetail.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import {defineAsyncComponent, onBeforeMount, ref} from "vue";
 import {useRouter} from "vue-router";
+import {useI18n} from "vue-i18n";
 
+const {t} = useI18n();
 const NotFound = defineAsyncComponent(() => import('@/views/NotFound.vue'));
-const IndexEpBrowser = defineAsyncComponent(() => import('@/components/index/IndexEpisodeBrowser.vue'));
-const IndexElementBrowser = defineAsyncComponent(() => import('@/components/index/IndexElementBrowser.vue'));
+const Tracks = defineAsyncComponent(() => import('@/components/index/IndexEpisodeBrowser.vue'));
+const Items = defineAsyncComponent(() => import('@/components/index/IndexElementItemBrowser.vue'));
 
-const prefix = 'entity-detail';
+const prefix = 'index-detail';
 const router = useRouter();
-const list = ref<any>();
+const entity = ref<any>();
 const meta = ref<any>();
 
 onBeforeMount(() => {
   meta.value = router.currentRoute.value.meta;
   if (!meta.value.notFound) {
-    list.value = meta.value.info;
+    entity.value = meta.value.info;
   }
 });
 </script>
@@ -25,63 +27,67 @@ onBeforeMount(() => {
     <div :class="`${prefix}`">
       <div :class="`${prefix}-main`">
         <div :class="`${prefix}-list-info`">
-          <div class="flex flex-col">
-            <div>
-              <div :class="`${prefix}-list-name`">{{ list.name }}</div>
-              <div :class="`${prefix}-list-log`">
-                <div>
-                  <RIcon name="person" :size="1.4"/>
-                  <span>{{ list.createdBy }}</span>
-                </div>
-                <div>
-                  <RIcon name="calendar_add_on" :size="1.4"/>
-                  <span>{{ list.createdAt }}</span>
-                </div>
-                <div>
-                  <RIcon name="edit_calendar" :size="1.4"/>
-                  <span>{{ list.updatedAt }}</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <div :class="`${prefix}-list-name`">{{ entity.name }}</div>
+          <article>{{ entity.remark }}</article>
+          <span style="color: #2ea6ff">{{ entity.createdBy }}</span>
+          <span style="color: #808080">•{{ t('CreatedAt') }}&nbsp;</span>
+          <span style="color: #B0C4DE">{{ entity.createdAt }}</span>
+          <span style="color: #808080">•{{ t('UpdatedAt') }}&nbsp;</span>
+          <span style="color: #B0C4DE">{{ entity.updatedAt }}</span>
         </div>
       </div>
     </div>
-    <IndexEpBrowser :listId="list.id" v-if="list.type.value === $const.ENTITY.EPISODE"/>
-    <IndexElementBrowser :indexId="list.id" v-if="list.type.value === $const.ENTITY.ITEM"/>
+    <Tracks :listId="entity.id" v-if="entity.type.value === $const.ENTITY.EPISODE"/>
+    <Items :indexId="entity.id" v-if="entity.type.value === $const.ENTITY.ITEM"/>
   </div>
 </template>
 
 <style scoped lang="scss">
-.entity-detail-main {
-  width: 106rem !important;
-}
+.index-detail {
+  display: flex !important;
+  flex-wrap: wrap !important;
+  justify-content: center !important;
 
-.entity-detail-list {
-
-  &-info {
-    background: var(--r-bg-indigo-700);
-    border-radius: 1rem;
-    margin: 1rem;
-    padding: 2rem;
+  article {
+    color: #d8d8d8 !important;
+    margin-bottom: .5rem;
   }
 
-  &-name {
-    font-weight: 700 !important;
-    font-size: 1.75rem !important;
-    margin-bottom: 2rem !important;
+  span {
+    font-size: 1.1rem;
   }
 
-  &-log {
-    display: flex !important;
-    flex-wrap: wrap !important;
-    gap: 5rem !important;
+  &-main {
+    width: 106rem !important;
+  }
 
-    > div {
-      display: flex !important;
-      align-items: center !important;
-      gap: 0.5rem !important;
+  &-list {
+
+    &-info {
+      background: var(--r-bg-indigo-700);
+      border-radius: 1rem;
+      padding: 1.5rem;
     }
+
+    &-name {
+      color: var(--r-sand-200);
+      font-weight: 700 !important;
+      font-size: 2rem !important;
+      margin-bottom: .5rem !important;
+    }
+
+    &-log {
+      display: flex !important;
+      flex-wrap: wrap !important;
+      gap: 5rem !important;
+
+      > div {
+        display: flex !important;
+        align-items: center !important;
+        gap: 0.5rem !important;
+      }
+    }
+
   }
 }
 </style>
